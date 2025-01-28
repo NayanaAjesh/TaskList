17d7a9bcb1e4b4fe90b977504beb0599bd7a4a28
Nayana <nayanasanse@gmail.com>
2025-01-28 13:18:23 +0530

    ssh nayana updated

--- src/Pages/Task.tsx
+++ src/Pages/Task.tsx
@@ -15,7 +15,7 @@ function Task() {
 
     const handleTask = () => {
         setTask([...task, {name: newTask, priority: priority}]);
-        console.log('Switch ssh', task);
+        console.log('Switch nayana ssh', task);
     }
     useEffect(() => {
         console.log('new', newTask, task);
